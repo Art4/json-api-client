9be29e3d7eea0f074e65d753bf428ea243634ed5
Art4 <art4@wlabs.de>
2015-08-10 15:37:26 +0200

    Added simple resource identifier test

---
+++ tests/files/02_simple_resource_identifier.js
@@ -0,0 +1,6 @@
+{
+  "data": {
+    "type": "articles",
+    "id": "1"
+  }
+}
--- tests/integration/IntegrationTest.php
+++ tests/integration/IntegrationTest.php
@@ -45,4 +45,28 @@ class IntegrationTest extends \PHPUnit_Framework_TestCase
 		$this->assertTrue($resource->hasRelationships());
 		$this->assertInstanceOf('Art4\JsonApiClient\RelationshipCollection',  $resource->getRelationships());
 	}
+
+	/**
+	 * @test parse() with valid JSON API returns Document Object
+	 */
+	public function testParseSimpleResourceIdentifier()
+	{
+		$string = $this->getJsonString('02_simple_resource_identifier.js');
+		$document = Helper::parse($string);
+
+		$this->assertInstanceOf('Art4\JsonApiClient\Document', $document);
+		$this->assertFalse($document->hasErrors());
+		$this->assertFalse($document->hasMeta());
+		$this->assertFalse($document->hasJsonapi());
+		$this->assertFalse($document->hasLinks());
+		$this->assertFalse($document->hasIncluded());
+		$this->assertTrue($document->hasData());
+
+		$resource = $document->getData();
+
+		$this->assertInstanceOf('Art4\JsonApiClient\ResourceIdentifier', $resource);
+		$this->assertFalse($resource->hasMeta());
+		$this->assertSame($resource->getType(), 'articles');
+		$this->assertSame($resource->getId(), '1');
+	}
 }
