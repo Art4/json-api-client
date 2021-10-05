be52db27b155cddd4ebe9f1a707e75ee1726829c
Art4 <art4@wlabs.de>
2021-10-05 14:56:39 +0200

    Add php-cs-fixer

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,13 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in('src')
+    ->in('tests')
+;
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@PSR12' => true,
+    ])
+    ->setFinder($finder)
+;
