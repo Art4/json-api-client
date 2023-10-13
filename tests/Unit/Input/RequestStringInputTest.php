25862b4d1069f5afe73158366f3537e4f617db77
Art4 <art4@wlabs.de>
2023-10-13 11:02:47 +0200

    switch to SPDX copyright style in src and tests

--- tests/Unit/Input/RequestStringInputTest.php
+++ tests/Unit/Input/RequestStringInputTest.php
@@ -1,21 +1,8 @@
 <?php
-/*
- * A PHP Library to handle a JSON API body in an OOP way.
- * Copyright (C) 2015-2021  Artur Weigandt  https://wlabs.de/kontakt
 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+// SPDX-FileCopyrightText: 2015-2023 Artur Weigandt https://wlabs.de/kontakt
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
 
 namespace Art4\JsonApiClient\Tests\Unit\Input;
 
